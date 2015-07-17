76b988ef2a3bf5e7f2175960af3e665c0da5b542
Konstantin <inferno2minus@gmail.com>
2015-07-17 22:00:49 +0500

    Исправлена ошибка начальной инициализации ExtraCycle

--- phoenix/phoenix.ino
+++ phoenix/phoenix.ino
@@ -461,7 +461,7 @@ void ServoDriver() {
     ServoDriverUpdate();
 
     //Finding any the biggest value for GaitPos/Rot
-    uint8_t ExtraCycle;
+    uint8_t ExtraCycle = 0;
     for (uint8_t LegIndex = 0; LegIndex <= 5; LegIndex++) {
       if ((GaitPosX[LegIndex] > 2) || (GaitPosX[LegIndex] < -2) ||
           (GaitPosZ[LegIndex] > 2) || (GaitPosZ[LegIndex] < -2) ||
